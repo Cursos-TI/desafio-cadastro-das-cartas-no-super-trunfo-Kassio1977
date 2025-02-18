036294bcfbd46b5f0af5fa447b045f11005c520c
Kassio1977 <troiax@gmail.com>
2025-02-18 20:21:03 -0300

    Update CartasSuperTrunfo.c
    Criação dos printf e scanf criação de cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,47 @@
 //Teste larissa
 
 int main() {
+   
+    char codigo[3];
+    char nome[20];
+    int populacao;
+    float area;
+    float pib;
+    int turismo;
+    float densidade;
+    float percapita;
+
+        printf("Digite o código da cidade: \n");
+        scanf("%s", &codigo);
+        
+        printf("Digite o nome da cidade: \n");
+        scanf("%s", &nome);
+        
+        printf("Digite a População: \n");
+        scanf("%d", &populacao);
+        
+        printf("Digite a área: \n");
+        scanf("%f", &area);
+        
+        printf("Digite o PIB: \n");
+        scanf("%f", &pib);
+        
+        printf("Digite o número de pontos turísticos: \n");
+        scanf("%d", &turismo);
+        
+        printf("Código: %s\n", codigo);
+        printf("Nome: %s\n", nome);
+        printf("População: %d\n", populacao);
+        printf("Area: %f\n", area);
+        printf("PIB: %.2f\n", pib);
+        printf("Número de pontos turísticos: %d\n", turismo);
+
+            densidade = (populacao / area);
+            percapita = (pib / populacao);
+
+            printf("Densidade populacional: %.2f\n", densidade);
+            printf("PIB per capita: %.2f\n", percapita);
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
